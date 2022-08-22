01dc46876a03bcc1537e6a588ca23780cd632bcb
Anmy Moreno <nayle2701@gmail.com>
2022-08-22 17:05:34 -0400

    dark

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ function App() {
   const [users, setUsers] = useState()
   const [updateInfo, setUpdateInfo] = useState()
   const [isOpen, setIsOpen] = useState(false)
-  const [themeMode, setThemeMode] = useState('dark')
+  const [themeMode, setThemeMode] = useState('')
+  const [icon, setIcon] = useState('bx-moon')
   //console.log(updateInfo)
   
   const getAllUsers = () => {
@@ -29,13 +30,22 @@ function App() {
    
   const handleCloseForm =() => setIsOpen(false)
   
-
+  const themeModeSwitch = () => {
+    if(themeMode === ''){
+      setThemeMode('dark')
+      setIcon('bx-sun')
+    }else{
+      setThemeMode('')
+      setIcon('bx-moon')
+    }
+  }
+  
   return (
     <div className={`App ${themeMode}`}>
       <div className="header">
         <h1>Users</h1>
         <button onClick={handleOpenForm}> + Create</button>
-        <button className='btn-dark'> DarkMode </button>
+        <button onClick={themeModeSwitch} className='btn-dark'><i className={`bx ${icon}`}></i></button>
       </div>
       <div className={isOpen ? 'form-container': 'form-none'}>
         <Form 
